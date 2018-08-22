6f99577c870cb844e39358cdd17cecb865b140ad
Derek Hammer and James Hanley <derek@>
2018-08-22 08:45:19 -0700

    init commit, add framework outline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
+import { AdminComponent } from './admin/admin.component';
+import { CreateComponent } from './create/create.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    WelcomeComponent,
+    ListComponent,
+    DetailsComponent,
+    AdminComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule
